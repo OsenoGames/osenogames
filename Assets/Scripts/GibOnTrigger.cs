b421479282d0270dd57ebb0db23bf9ed6f75c468
Jake Whited <main@osenogames.com>
2014-12-25 15:17:41 -0500

    Added enemy and explosion the the level. Also wanted to see Version control working.

---
+++ Assets/Scripts/GibOnTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GibOnTrigger : MonoBehaviour 
+{
+	public GameObject gibs;
+
+	void OnTriggerEnter()
+	{
+		Instantiate(gibs,transform.position,Quaternion.identity);
+		Destroy(gameObject);
+	}
+}
