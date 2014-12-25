15a2adc55fa299ac36723ba6e61b16b88a9e8646
Jake Whited <main@osenogames.com>
2014-12-25 07:20:44 -0500

    First Commit

---
+++ Assets/Scripts/MoveLeftandRight.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLeftandRight : MonoBehaviour 
+{
+	public float speed = 1.0f;
+
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+		//this moves the object left and right based on input and float speed
+		transform.position += Vector3.right*Input.GetAxis("Horizontal")*speed*Time.deltaTime;
+	}
+}
