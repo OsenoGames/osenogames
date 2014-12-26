94d80918a506680efae95e9b12f440538f2596ad
Jake Whited <main@osenogames.com>
2014-12-25 21:32:08 -0500

    Added skybox and animated texture to level1.

---
+++ Assets/Scripts/AnamateTexture.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnamateTexture : MonoBehaviour 
+{
+	public Vector2 speed = Vector2.one;
+	string mve;
+
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+	renderer.material.mainTextureOffset += speed * Time.deltaTime;
+	}
+}
