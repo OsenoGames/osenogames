f7563f8c9171c5f9b63fb72ecf92d115202a4082
Jake Whited <main@osenogames.com>
2014-12-27 05:17:19 -0500

    Created a game manager and tem scoring system. Changed Skybox and lighting level1.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour 
+{
+	public static GameManager Instance;
+	private static float highScore = 0.0f;
+	public float pointPerUnit = 1.0f;
+	private bool gameOver = false;
+	private float score = 0.0f;
+	public float gameSpeed = 20.0f;
+
+	void Start () 
+	{
+		Instance = this;
+		//Coroutine ("onGUI");
+	}
+	
+	void Update () 
+	{
+		if(!gameOver)
+		{
+			score += pointPerUnit*gameSpeed*Time.deltaTime;
+			if(score>highScore)
+			{
+				highScore = score;
+			}
+		}
+
+		if(GameObject.FindGameObjectWithTag("Player") == null)
+		{
+			gameOver = true;
+			//Coroutine ("onGUI");
+
+		}
+		if(gameOver)
+		{
+			if(Input.anyKeyDown)
+			{
+				Application.LoadLevel(Application.loadedLevel);
+			}
+
+		}
+
+	}
+	void OnGUI()
+	{
+		//int currentScore = (int)score;
+		//GUI.Label(new Rect(10, 10, 50, 25), "Name: ");
+		//GUI.Label(new Rect(50, 50, 100, 25), "Score " + currentScore.ToString());
+		GUILayout.Label("Score: " + ((int)score).ToString());
+		GUILayout.Label("High Score: " + ((int)highScore).ToString());
+		if(gameOver == true)
+		{
+			GUILayout.Label("Game Over!  Press any key to restart...");
+		}
+	}
+}
---
+++ Assets/Scripts/MoveBasedOnGameSpeed.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBasedOnGameSpeed : MonoBehaviour 
+{
+	public Vector3 direction;
+
+
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+		transform.position += transform.rotation*(direction.normalized*GameManager.Instance.gameSpeed*Time.deltaTime);
+	}
+}
