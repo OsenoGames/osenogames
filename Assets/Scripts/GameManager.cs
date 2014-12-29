3046a5916404a847150d15721133af95b731f404
Jake Whited <main@osenogames.com>
2014-12-29 02:39:19 -0500

    Working on High score system. Still needs work but is work so far.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,6 +13,12 @@ public class GameManager : MonoBehaviour
 	private bool gameOver = false;
 	private float score = 0.0f;
 	private bool hasSaved = false;
+	int hs1 = 0;
+	int hs2 = 0;
+	int hs3 = 0;
+	int hs4 = 0;
+	int hs5 = 0;
+	int scin = 0;
 
 
 	void Start () 
@@ -71,12 +77,58 @@ public class GameManager : MonoBehaviour
 	}
 	void SaveHighScore()
 	{
-		PlayerPrefs.SetInt("HighScore", (int)highScore);
-		PlayerPrefs.Save();
+		scin = (int)score;
+		if(scin >= hs1)
+		{
+			PlayerPrefs.SetInt("H5", hs4);
+			PlayerPrefs.SetInt("H4", hs3);
+			PlayerPrefs.SetInt("H3", hs2);
+			PlayerPrefs.SetInt("H2", hs1);
+			PlayerPrefs.SetInt("H1", scin);
+			PlayerPrefs.Save();
+			scin = 0;
+		}
+		if(scin >= hs2)
+		{
+			PlayerPrefs.SetInt("H5", hs4);
+			PlayerPrefs.SetInt("H4", hs3);
+			PlayerPrefs.SetInt("H3", hs2);
+			PlayerPrefs.SetInt("H2", scin);
+			PlayerPrefs.Save();
+			scin = 0;
+		}
+		if(scin >= hs3)
+		{
+			PlayerPrefs.SetInt("H5", hs4);
+			PlayerPrefs.SetInt("H4", hs3);
+			PlayerPrefs.SetInt("H3", scin);
+			PlayerPrefs.Save();
+			scin = 0;
+		}
+		if(scin >= hs4)
+		{
+			PlayerPrefs.SetInt("H5", hs4);
+			PlayerPrefs.SetInt("H4", scin);
+			PlayerPrefs.Save();
+			scin = 0;
+		}
+		if(scin >= hs5)
+		{
+			PlayerPrefs.SetInt("H5", scin);
+			PlayerPrefs.Save();
+			scin = 0;
+		}
+		//PlayerPrefs.SetInt("HighScore", (int)highScore);
+		//PlayerPrefs.Save();
 	}
 	void LoadHighScore()
 	{
 		highScore = PlayerPrefs.GetInt("HighScore");
+		hs1 = PlayerPrefs.GetInt("H1",0);
+		hs2 = PlayerPrefs.GetInt("H2",0);
+		hs3 = PlayerPrefs.GetInt("H3",0);
+		hs4 = PlayerPrefs.GetInt("H4",0);
+		hs5 = PlayerPrefs.GetInt("H5",0);
 	}
 
 	void OnGUI()
