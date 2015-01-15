ba78d1154bdab099b5800856e3d3d446e9ed19ad
Jake Whited <main@osenogames.com>
2015-01-14 22:02:50 -0500

    Added TTF and skybox

---
+++ Assets/Scripts/TimeTest.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeTest : MonoBehaviour 
+{
+	float gameTime;
+	int totalHours;
+	int totalMin;
+	int totalSec;
+	bool oneTime = true;
+
+	void Start () 
+	{
+	gameTime = 0.0f;
+	}
+	
+	void Update () 
+	{
+		if(GameManager.Instance.gameOver)
+		{
+			if(oneTime)
+			{
+				oneTime = false;
+				TotalTime();
+
+			}
+		}
+	}
+
+	void TotalTime()
+	{
+		gameTime = Time.time;
+		Debug.Log("Total Time " + gameTime);
+		totalHours = (int)gameTime / 3600;
+		gameTime = gameTime - totalHours * 3600;
+		totalMin = (int)gameTime / 60;
+		totalSec = (int)gameTime - (int)totalMin * 60;
+		
+		Debug.Log("Total Hours " + totalHours);
+		Debug.Log("Total Mins " + totalMin);
+		Debug.Log("Total Secs " + totalSec);
+	}
+	void OnGUI(){
+		if(GameManager.Instance.gameOver)
+			GUILayout.Label(string.Format("{0:00}:{1:00} with",totalMin + (totalHours), totalSec));
+	}
+		
+
+}
