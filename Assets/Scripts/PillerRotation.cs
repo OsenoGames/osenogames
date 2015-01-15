436f66c310a8e3b5e817cc614b546533af3559e7
Jake Whited <main@osenogames.com>
2015-01-15 04:53:50 -0500

    added new speeder and colliders

--- Assets/Scripts/PillerRotation.cs
+++ Assets/Scripts/PillerRotation.cs
@@ -5,10 +5,16 @@ public class PillerRotation : MonoBehaviour
 {
 	public int speed = 10;
 
+	void Start()
+	{
+		//speed = Random(1, 100);
+		//transform.RotateAround(Vector3)
+	}
 	void Update () 
 	{
 		if(GameManager.Instance.gameOver == false)
 		{
+			//transform.RotateAround(Vector3.up, 1 * Time.deltaTime);
 			transform.Rotate(Vector3.up * speed * Time.deltaTime);
 		}
 	}
