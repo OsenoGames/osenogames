ced4fb29e49dc345ea545806ac6cf81310c1453e
Jake Whited <main@osenogames.com>
2015-01-16 06:24:08 -0500

    fixed credits screen and others. added more music

---
+++ Assets/Scripts/Menu/Credits.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour 
+{
+	public bool startLevel = false;
+
+	public void Click1(bool startLevel)
+	{
+		if(startLevel)
+			Application.LoadLevel("Credits");
+	}
+
+}
